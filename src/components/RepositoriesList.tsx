e4cc919bcd3c13e7ff55e94af61cd0f8799f0a00
Cai <trieungochai.dev@gmail.com>
2021-08-18 02:51:39 +0700

    Consuming Store State

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -20,6 +20,11 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error &&
+        !loading &&
+        data.map((name: any) => <div key={name}>{name}</div>)}
     </div>
   );
 };
