58392aeaefafccdb4d0499fcce6c6f21d22c90de
Cai <trieungochai.dev@gmail.com>
2021-08-17 19:32:33 +0700

    Adding Action Creators

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { ActionType } from "../action-types";
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPOSITORIES,
+    });
+
+    try {
+      const { data } = await axios.get(
+        "https://registry.npmjs.org/-/v1/search",
+        {
+          params: term,
+        }
+      );
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (error) {
+      dispatch({
+        type: ActionType.SEARCH_REPOSITORIES_ERROR,
+        payload: error.message,
+      });
+    }
+  };
+};
